baddf30464e79f9b622682574d1e94162862e19e
Arjun <arjunaravind2001@gmail.com>
2024-09-25 20:12:54 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-3xl font-bold underline bg-black">Hello, Tailwind CSS with Vite!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
